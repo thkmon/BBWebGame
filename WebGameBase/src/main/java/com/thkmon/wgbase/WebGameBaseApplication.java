01c7dc246b6a8a50d9add81f0638fe0d00f3f92e
thkmon <thkmon@gmail.com>
2020-11-12 03:13:21 +0900

    201112. Initial Commit.

---
+++ WebGameBase/src/main/java/com/thkmon/wgbase/WebGameBaseApplication.java
@@ -0,0 +1,12 @@
+package com.thkmon.wgbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGameBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGameBaseApplication.class, args);
+	}
+}
---
+++ WebGameBase/src/test/java/com/thkmon/wgbase/WebGameBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thkmon.wgbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGameBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
