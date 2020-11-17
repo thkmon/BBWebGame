b4eaad73c5ecf78f6a951590a2ca90a65b8124af
thkmon <thkmon@gmail.com>
2020-11-17 23:38:50 +0900

    201117. websocket 작업

--- WebGameBase/src/main/java/com/thkmon/wgbase/MainController.java
+++ WebGameBase/src/main/java/com/thkmon/wgbase/MainController.java
@@ -1,7 +1,11 @@
 package com.thkmon.wgbase;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
@@ -9,5 +13,12 @@ public class MainController {
 	@RequestMapping(value="/")
 	public String main() {
 		return "index";
+	}
+	
+	
+	// websocket
+	@RequestMapping(value = "/chat", method = RequestMethod.GET)
+	public String chat(Locale locale, Model model) {
+		return "chat";
 	}
 }
--- WebGameBase/src/main/java/com/thkmon/wgbase/WebGameBaseApplication.java
+++ WebGameBase/src/main/java/com/thkmon/wgbase/WebGameBaseApplication.java
@@ -2,11 +2,20 @@ package com.thkmon.wgbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class WebGameBaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebGameBaseApplication.class, args);
+	}
+	
+	
+	// websocket
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
 	}
 }
